fadf32022c8e32515b7ab567d24cc459c3f2518f
Logon Aluno <logonaluno@local.com>
2019-08-06 09:56:39 -0300

    first commit, navmesh, and movimentation

---
+++ Assets/SetDestination.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SetDestination : MonoBehaviour
+{
+    //componets
+    public Transform alvo;
+    public NavMeshAgent navMeshAgent;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        Movimentar();
+    }
+
+    private void Movimentar()
+    {
+        //you could use the navMeshAgent.SetDestination, but we are using the .destinantion-
+        navMeshAgent.destination = alvo.position;
+    }
+}
